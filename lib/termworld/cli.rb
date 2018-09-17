0b090b957fc040b3189325099d16de389c352724
kthatoto <kthatoto@gmail.com>
2018-09-17 18:26:30 +0900

    Added sqlite

--- lib/termworld/cli.rb
+++ lib/termworld/cli.rb
@@ -1,5 +1,6 @@
 require "termworld"
 require "thor"
+require "sqlite3"
 
 module Termworld
   class CLI < Thor
@@ -7,7 +8,15 @@ module Termworld
     desc "plant", "Plang seed"
     def plant
       init
-      puts "Planted!!"
+      puts "Planted!"
+    end
+
+    desc "farming", "Farming"
+    def farming
+      init
+      loop do
+        sleep 1
+      end
     end
 
     no_commands do
@@ -16,6 +25,16 @@ module Termworld
         unless Dir::exists?(".termworld")
           Dir::mkdir(".termworld")
         end
+        Dir::chdir(".termworld")
+        db = SQLite3::Database.new "termworld.db"
+        tables = <<-SQL
+          create table users (id integer primary key);
+          create table plants (
+            id integer primary key,
+            growth integer default 0
+          );
+        SQL
+        db.execute(tables)
       end
     end
   end
