60d505a9d315575ef2776a24068bf1df3a05141c
kthatoto <kthatoto@gmail.com>
2018-09-17 16:57:40 +0900

    Added mkdir

--- lib/termworld/cli.rb
+++ lib/termworld/cli.rb
@@ -3,9 +3,20 @@ require "thor"
 
 module Termworld
   class CLI < Thor
-    desc "helloworld", "say helloworld"
-    def helloworld
-      puts `pwd`
+
+    desc "plant", "Plang seed"
+    def plant
+      init
+      puts "Planted!"
+    end
+
+    no_commands do
+      def init
+        Dir::chdir(Dir::home)
+        unless Dir::exists?(".termworld")
+          Dir::mkdir(".termworld")
+        end
+      end
     end
   end
 end
